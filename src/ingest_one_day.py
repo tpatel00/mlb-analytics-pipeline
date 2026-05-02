accd1f4c127d23dd8f9fb23d995a50e552d3c71a
tpatel00 <tepatel@ucsd.edu>
2026-05-01 17:14:32 -0700

    Add CLI date argument for ingestion script

--- src/ingest_one_day.py
+++ src/ingest_one_day.py
@@ -1,1 +1,12 @@
-print("MLB pipeline script is connected.")
+import argparse
+
+parser = argparse.ArgumentParser(description="Date to ingest data for (YYYY-MM-DD)")
+parser.add_argument('--date', type=str, required=True,
+                    help= "Date to ingest data for (YYYY-MM-DD)") # Dates will be string, REQUIRE a date when running
+
+args = parser.parse_args() # Get the arguments
+
+run_date = args.date # assign date argument to run_date
+
+print(f"Running MLB ingestion for date: {run_date}")
+
