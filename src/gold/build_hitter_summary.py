3bac2ce20debff969fcf3ec70677590b521f92cb
tpatel00 <tepatel@ucsd.edu>
2026-05-14 21:03:29 -0700

    started gold hitter summary table

--- src/gold/build_hitter_summary.py
+++ src/gold/build_hitter_summary.py
@@ -1,1 +1,7 @@
 # Build out gold hitter summary table
+import pandas as pd
+import numpy as np
+from pybaseball import playerid_reverse_lookup
+
+df = pd.read_parquet("../data/silver/pitches/pitches_2024-04-01.parquet") # Read pitch data parquet file
+
